13f458c48b08c4b51334cf6f04a54b5f33d017e4
pc <www.mmhmdmm83@gmail.com>
2022-04-01 17:54:02 +0430

    add Top btn in website

--- src/components/ButtonTop/ButtonTop.js
+++ src/components/ButtonTop/ButtonTop.js
@@ -1,9 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
 import "./ButtonTop.css";
 
 export default function ButtonTop() {
+  const [topBtnShow, setTopBtnShow] = useState(false);
+
+  window.onscroll = () => {
+    if (window.scrollY > 200) {
+      setTopBtnShow(true);
+    } else {
+      setTopBtnShow(false);
+    }
+  };
+
+  const scrollHandler = () => {
+    window.scrollTo(0, 0);
+  };
   return (
-    <div className="top-btn">
+    <div
+      onClick={scrollHandler}
+      className={`top-btn ${topBtnShow && "active"}`}
+    >
       <span className="fas fa-angle-up"></span>
     </div>
   );
